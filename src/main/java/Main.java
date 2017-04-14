9b965a108b3f079a74bee1c559670cc5e1c90664
Gil Vegliach <gil.vegliach@gmail.com>
2017-04-14 13:55:48 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    for (int i = 1; i <= t; i++) {
+      System.out.printf("Case #%d: %s\n", i, solution());
+    }
+  }
+
+  static String solution() {
+    return "OK";
+  }
+}
+
+
+
+
+
+
+
+
